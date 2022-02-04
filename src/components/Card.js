ac8f19414a4a37de98a8fce691b2556ce29f30c0
MarcoSiabatto <siabattorodriguezmarco@gmail.com>
2022-02-04 11:38:04 -0500

    BuiltBasicCard

---
+++ src/components/Card.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Katie from "../images/katie-zaferes.png";
+import Star from "../images/star.png";
+
+const Card = () => {
+  return (
+    <div className="card">
+      <img src={Katie} alt="Katie-zaferes" className="card--image" />
+      <div className="card--stats">
+        <img src={Star} alt="star" className="card--star" />
+        <span>5.0</span>
+        <span className="gray">(6) * </span>
+        <span className="gray">USA</span>
+      </div>
+      <p>Life Lessons with Katie Zaferes</p>
+      <p>
+        <span className="bold">From $136</span> / person
+      </p>
+    </div>
+  );
+};
+
+export default Card;
